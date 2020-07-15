95656154b45d75218e51ac0665eb60e9f52474e0
刘天佑@洛谷 <61583168+LiuTianyouOnLuogu@users.noreply.github.com>
2020-07-15 17:02:20 +0800

    更新了cmpl.sh
    添加了对多个命令行的支持（如cmsl.sh -Werror -well）

--- code/cmpl.sh
+++ code/cmpl.sh
@@ -16,11 +16,11 @@ CPP="cpp"
 if [ $suffix == $CPP ];then 
 	# Check the compiler and compile
 	if command -v clang++ >/dev/null 2>&1; then 
-  		clang++ $1 -o ../tests/$filename.exe 
+  		clang++ $(echo $# |sed 's/$0//g') -o ../tests/$filename.exe
 	else 
     		echo -e 'no exists clang, check gcc...\n' 
     		if command -v g++ >/dev/null 2>&1; then 
-      			g++ $1 -o ../tests/$filename.exe 
+      			g++ $(echo $# |sed 's/$0//g') -o ../tests/$filename.exe 
     		else 
         		echo 'no exists compiler'
 			exit
@@ -33,7 +33,7 @@ fi
 if [ $suffix == $RUST ];then
 	# Check the compiler and compile
 	if command -v rustc >/dev/null 2>&1; then
-		rustc $1 -o ../tests/$filename.exe
+		rustc $(echo $# |sed 's/$0//g') -o ../tests/$filename.exe
 	else
 		echo 'no exists rustc'
 	fi
