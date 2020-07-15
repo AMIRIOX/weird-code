e53bbb0525052e7b579a0bc0a8224796ac1853e5
刘天佑@洛谷 <61583168+LiuTianyouOnLuogu@users.noreply.github.com>
2020-07-15 21:31:47 +0800

    Create undefined-behavior.cpp

---
+++ code/undefined-behavior.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+void print(int a,b){
+  cout<<a<<" "<<b<<endl;
+  printf("%d %d\n",a,b);
+}
+void good(){
+  thread t(good);
+  t.join();
+}
+int main(){
+  long long a;
+  ios::sync_with_stdio(false);
+  cout<<(double)1.0/0<<" "<<(float)1.0/0<<endl;
+  cout<<a<<endl;
+  int fxxk_array[-2147483647];
+  cout<<fxxk_array[-123]<<endl;
+  good();//fxxk
+  return -1;
+}
