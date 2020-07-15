1955aa7ff39b2340b7410450560a2ff3de81f88d
刘天佑@洛谷 <61583168+LiuTianyouOnLuogu@users.noreply.github.com>
2020-07-15 17:07:11 +0800

    增加了编译错误的情况，等我回家改成configure文件。

--- code/cmpl.sh
+++ code/cmpl.sh
@@ -16,14 +16,14 @@ CPP="cpp"
 if [ $suffix == $CPP ];then 
 	# Check the compiler and compile
 	if command -v clang++ >/dev/null 2>&1; then 
-  		clang++ $(echo $# |sed 's/$0//g') -o ../tests/$filename.exe
+  		clang++ $(echo $# |sed 's/$0//g') -o ../tests/$filename.exe || exit $?
 	else 
     		echo -e 'no exists clang, check gcc...\n' 
     		if command -v g++ >/dev/null 2>&1; then 
-      			g++ $(echo $# |sed 's/$0//g') -o ../tests/$filename.exe 
+      			g++ $(echo $# |sed 's/$0//g') -o ../tests/$filename.exe || exit $?
     		else 
         		echo 'no exists compiler'
-			exit
+			exit 2
 	    	fi
 	fi
 	../tests/$filename.exe
@@ -33,7 +33,7 @@ fi
 if [ $suffix == $RUST ];then
 	# Check the compiler and compile
 	if command -v rustc >/dev/null 2>&1; then
-		rustc $(echo $# |sed 's/$0//g') -o ../tests/$filename.exe
+		rustc $(echo $# |sed 's/$0//g') -o ../tests/$filename.exe || exit $?
 	else
 		echo 'no exists rustc'
 	fi
