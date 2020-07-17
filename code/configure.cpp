01cd99f59cfb4544e23f21c782a0c86a629a3b40
刘天佑@洛谷 <61583168+LiuTianyouOnLuogu@users.noreply.github.com>
2020-07-17 10:40:45 +0800

    Update configure.cpp
    改变了configure.cpp的用法。

--- code/configure.cpp
+++ code/configure.cpp
@@ -2,25 +2,32 @@
 using namespace std;
 int main(int argc,char** argv){
 #ifdef linux
-    ostream autocompile("../AutoCompile.sh ");
+    ofstream autocompile("../AutoCompile.sh ");
     system("chmod 777 ./AutoCompile.sh");
     autocompile<<"#!/bin/bash"<<endl<<"# Configure Script: Hello world!"<<endl;
 #else
-    ostream autocompile("../AutoCompile.bat");
+    ofstream autocompile("../AutoCompile.bat");
     autocompile<<"@echo off"<<endl<<"rem Configure Script: Hello world!"<<endl;
 #endif
-    if(argc%3!=1){
-        bool first_time=true,a_time=false;
-        string compiler,files,commandline,i;
-        while(true){
-            cin>>compiler>>files;
-            while(true){
-                cin>>i;
-                if((i.find("\"")==i.npos&&first_time==true)||(i.find("\"")!=i.npos&&a_time==true)) {commandline+=i;break;}
-                else if(i.find("\"")!=i.npos&&a_time==false) a_time=true;
-                commandline+=i;
-                first_time=false;
-            }
+    if(!strcmp(argv[1],"auto")){
+        if(!strcmp(argv[2],"windows")){
+            ofstream listFile("windows.list");
+        }
+        else if(!strcmp(argv[3],"linux")){
+            ofstream listFile("linux.list");
+        }
+        else{
+            ofstream listFile("compatible.list");
+        }
+        bool flag=true;
+    }
+    for(int i=1;i<argc;i+=3){
+        string compiler=argv[i];
+        string commandline=argv[i+2];
+        string i;
+        if(flag==false){
+            string files=argv[i+1];
+            while(sscanf(files.c_str(),"%s",i)!=0){
 #ifdef linux
                 autocompile<<"# Configure Script: Outputing the source code: "<<i<<endl;
 #else
@@ -28,22 +35,15 @@ int main(int argc,char** argv){
 #endif
                 autocompile<<compiler<<" "<<i<<" "<<commandline<<" ../bin/"<<i<<" || { echo \"Compile Error! Please go to github and submit a issue.\";exit $? }"<<endl;
             }
-            return 0;
-        }
-    }
-    for(int i=1;i<argc;i+=3){
-        string compiler=argv[i];
-        string files=argv[i+1];
-        string commandline=argv[i+2];
-        string i;
-        while(sscanf(files.c_str(),"%s",i)!=0){
+        }else{
+            while(listFile>>i){
 #ifdef linux
-            autocompile<<"# Configure Script: Outputing the source code: "<<i<<endl;
+                autocompile<<"# Configure Script: Outputing the source code: "<<i<<endl;
 #else
-            autocompile<<"rem Configure Script: Outputing the source code: "<<i<<endl;
+                autocompile<<"rem Configure Script: Outputing the source code: "<<i<<endl;
 #endif
-            autocompile<<compiler<<" "<<i<<" "<<commandline<<" ../bin/"<<i<<" || { echo \"Compile Error! Please go to github and submit a issue.\";exit $? }"<<endl;
-        }
+                autocompile<<compiler<<" "<<i<<" "<<commandline<<" ../bin/"<<i<<" || { echo \"Compile Error! Please go to github and submit a issue.\";exit $? }"<<endl;
+            }
     }
     return 0;
 }
